ab46a1bdf69dbcaff609ada68bfbb40ea748b56b
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-08-30 19:18:16 +0000

    Rating select component

--- feedbackapp/src/component/FeedbackForm.jsx
+++ feedbackapp/src/component/FeedbackForm.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [rating, setRating] = useState()
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
 
@@ -24,18 +26,20 @@ function FeedbackForm() {
     }
   return (
     <Card>
-        <h2>How would you rate your service with us?</h2>
-        {/* @todo = rating select Component */}
-        <div className="input-group">
-            <input 
-                onChange={handTextChange}
-                type="text"
-                placeholder='Write a review'
-                value={text} 
-            />
-            <Button type="submit" isDisabled={btnDisabled}>Send</Button>
-        </div>
-        {message && <div className='message'>{message}</div>}
+        <form>
+            <h2>How would you rate your service with us?</h2>
+            <RatingSelect select={(rating) => setRating(rating)} />
+            <div className="input-group">
+                <input 
+                    onChange={handTextChange}
+                    type="text"
+                    placeholder='Write a review'
+                    value={text} 
+                />
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+            </div>
+            {message && <div className='message'>{message}</div>}
+        </form>
     </Card>
   ) 
 }

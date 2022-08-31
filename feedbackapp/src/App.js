487faf9cd1ecbf0e07248730d7a0b5f29e12f43e
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-08-31 08:49:47 +0000

    Create React routes

--- feedbackapp/src/App.js
+++ feedbackapp/src/App.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './component/Header';
+import { icons } from 'react-icons/lib';
+
 import FeedbackList from './component/FeedbackList';
 import FeedbackStats from './component/FeedbackStats';
 import FeedbackForm from './component/FeedbackForm';
 import FeedbackData from './data/FeedbackData';
-import { icons } from 'react-icons/lib';
+import AboutPage from './Pages/AboutPage';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,14 +24,23 @@ function App() {
     }
   }
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete = {deleteFeedBack} />
+        <Routes>
+          <Route exact path='/' element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete = {deleteFeedBack} />
+            </>
+          } >   
+          </Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 

c14973352cb4efafc3891b010f624ec174c06bae
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-09-01 15:31:04 +0000

    Setting proxy

--- feedbackapp/src/context/FeedbackContext.js
+++ feedbackapp/src/context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext()
@@ -17,18 +16,26 @@ export const FeedbackProvider = ({children}) => {
 
     // Fetch Feedback
     const fetchFeedback = async() => {
-        const response = await fetch(`http://localhost:5000/feedbck?_sort=id&_order=desc`)
-        // const response = await fetch(`https://5000-saintjosh-reactfromtoba-44n581l8uh5.ws-eu63.gitpod.io/feedback?_sort=id&_order=desc`)
+        // const response = await fetch(`/feedback?_sort=id&_order=desc`)
+        const response = await fetch(`https://5000-saintjosh-reactfromtoba-44n581l8uh5.ws-eu63.gitpod.io/feedback?_sort=id&_order=desc`)
         const data = await response.json()
 
         setFeedback(data)
         setIsLoading(false)
     }
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
-      }
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('https://5000-saintjosh-reactfromtoba-44n581l8uh5.ws-eu63.gitpod.io/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+        setFeedback([data, ...feedback])
+    }
 
     const deleteFeedBack = (id) => {
         if(window.confirm('Are you sure you want to delete?')) {

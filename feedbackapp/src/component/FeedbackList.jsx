6e8d849740075d6d6f88110653d03450fe5a1a32
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-09-01 14:16:37 +0000

    spinner component

--- feedbackapp/src/component/FeedbackList.jsx
+++ feedbackapp/src/component/FeedbackList.jsx
@@ -2,32 +2,33 @@ import React from 'react';
 import { useContext } from 'react';
 import {motion, AnimatePresence} from 'framer-motion'
 import FeedbackItem from './FeedbackItem';
+import Spinner from './shared/Spinner';
 import Card from './shared/Card';
 import FeedbackContext from '../context/FeedbackContext';
 // import PropTypes from 'prop-types';
 
 function FeedbackList() {
-    const { feedback } = useContext(FeedbackContext)
-    if (!feedback || feedback.length === 0) {
+    const { feedback, isLoading } = useContext(FeedbackContext)
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback yet</p>
     }
     
     return (
-    <div className="feedback-list">
-        <AnimatePresence >
-            {feedback.map((item) => (
-                <motion.div
-                    key={item.id}
-                    initial={{opacity: 0}}
-                    animate={{opacity:1}}
-                    exit={{opacity:0}}
-                >
-                    <FeedbackItem key={item.id} item = {item} />
-                </motion.div>
-            ))}
-        </AnimatePresence>
-    </div>
-  )
+            <div className="feedback-list">
+                <AnimatePresence >
+                    {feedback.map((item) => (
+                        <motion.div
+                            key={item.id}
+                            initial={{opacity: 0}}
+                            animate={{opacity:1}}
+                            exit={{opacity:0}}
+                        >
+                            <FeedbackItem key={item.id} item = {item} />
+                        </motion.div>
+                    ))}
+                </AnimatePresence>
+            </div>
+        )
 }
 
 // FeedbackList.propTypes = {
